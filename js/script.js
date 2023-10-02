82078647ab78f1b3d76a618361285b3dd5b81121
Alécio <ciceroalecio30@gmail.com>
2023-10-02 19:06:40 -0300

    Desempenho

--- js/script.js
+++ js/script.js
@@ -1,2 +1,24 @@
-
+$(document).ready(function () {
+    const pontosDica = $(".ponto-dica");
+    const dicas = $(".dica");
+  
+    // Inicialmente, exiba a primeira dica e marque o primeiro ponto como ativo
+    mostrarDica(0);
+  
+    pontosDica.on("click", function () {
+      const indice = $(this).index();
+      
+      // Oculte todas as dicas e remova a classe ativa de todos os pontos
+      dicas.hide();
+      pontosDica.removeClass("ativo");
+  
+      // Mostre a dica selecionada e marque o ponto correspondente como ativo
+      mostrarDica(indice);
+      $(this).addClass("ativo");
+    });
+  
+    function mostrarDica(indice) {
+      dicas.eq(indice).show();
+    }
+  });
   
